f9f42bbd1f45d6fe3b1af4eb034666277740ddfb
Colin Davis <cdavis@darkenedsky.com>
2011-07-08 16:49:12 -0400

    sort

--- webfront.py
+++ webfront.py
@@ -297,7 +297,7 @@ class ImgHandler(tornado.web.RequestHandler):
         
         if client_bgset is not "":
             bglist = []
-            for ls in os.listdir(client_bgset):
+            for ls in os.listdir(client_bgset).sort():
                 if not ls.startswith("."):
                     bglist.append(client_bgset + "/" + ls)
             bg = Image.open(bglist[r.hasharray[3] % len(bglist)])
