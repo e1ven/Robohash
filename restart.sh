8cb8d7dde5d1bd86f36d6bd21d4efead2370400c
Colin Davis <colin@sq7.org>
2012-04-02 20:15:41 +0000

    Agnostic URL

---
+++ restart.sh
@@ -0,0 +1,5 @@
+killall python
+nohup ./webfront.py --port=8080 > /dev/null  & 
+nohup ./webfront.py --port=8081 > /dev/null & 
+nohup ./webfront.py --port=8082  > /dev/null & 
+nohup ./webfront.py --port=8083 > /dev/null &
